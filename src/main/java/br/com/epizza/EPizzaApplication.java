e168fd9e1b21277d07aace065b0c93bc6a9b5845
Jonatas <jonatascalmeida@gmail.com>
2019-02-11 21:11:00 -0200

    Alteracoes angular, botao add E rmv quantidade, labels segundo sabor

--- src/main/java/br/com/epizza/EPizzaApplication.java
+++ src/main/java/br/com/epizza/EPizzaApplication.java
@@ -1,12 +1,15 @@
 package br.com.epizza;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EPizzaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(EPizzaApplication.class, args);
 	}
 }
